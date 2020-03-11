089ffdb1e24ca073947463b8e9a531f3862aacbd
Alberto Martinez <martini73.aml@gmail.com>
2020-03-11 12:37:36 +0100

    Finished

---
+++ ExamenAlbertoMartinezCambioDivisas/Services/Log/LogConsole.cs
@@ -0,0 +1,22 @@
+﻿using ExamenAlbertoMartinezCambioDivisas.InfraestructuraTransversal.Exceptions;
+using System;
+
+namespace ExamenAlbertoMartinezCambioDivisas.Services.Log
+{
+    public class LogConsole : ILog
+    {
+        public void WriteLog(string message)
+        {
+            var date = DateTime.Now;
+
+            try
+            {
+                Console.WriteLine($"[{date}] {message}");
+            }
+            catch (Exception ex)
+            {
+                throw new LogException("No ha sido posible escribir el log en la consola: LogConsole.", ex);
+            }
+        }
+    }
+}
---
+++ ExamenAlbertoMartinezCambioDivisas/Services/Log/LogTxt.cs
@@ -0,0 +1,35 @@
+﻿using ExamenAlbertoMartinezCambioDivisas.InfraestructuraTransversal.Exceptions;
+using System;
+using System.IO;
+
+namespace ExamenAlbertoMartinezCambioDivisas.Services.Log
+{
+    //Escibir en TXt
+    public class LogTxt : ILog
+    {
+        private readonly string _path;
+        public LogTxt()
+        {
+            this._path = AppDomain.CurrentDomain.BaseDirectory + "LogGenerado";
+            Directory.CreateDirectory(this._path);
+
+        }
+        public void WriteLog(string message)
+        {
+            var date = DateTime.Now.ToString("dd/MM/yyyy");
+            var time = DateTime.Now.ToString("HH:mm:ss");
+
+            using (var sw = new StreamWriter(this._path + "/logfile.txt", true))
+            {
+                try
+                {
+                    sw.WriteLine("[" + date + " " + time + "] " + message);
+                }
+                catch (Exception ex)
+                {
+                    throw new LogException("No ha sido posible escribir el log en el fichero: LogTxt.", ex);
+                }
+            }
+        }
+    }
+}
